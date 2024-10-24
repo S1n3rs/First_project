d9a9ccfd44699b40e0e76a57b3178bd7a98fc311
S1n3rs <aunker13@gmail.com>
2024-10-24 21:14:19 +0500

    Add files via upload
    Первый комит

---
+++ ConsoleApplication35/ConsoleApplication35.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+double PI = 3.14;
+
+class okrujnost
+{
+private:
+    double r;
+public:
+    okrujnost(double radius) : r(radius) {}
+    double okr()
+    {
+        return 2 * PI * r;
+    }
+
+    double area()
+    {
+        return PI * r * r;
+    }
+};
+
+int main()
+{
+    setlocale(0, "");
+    double r;
+    cout << "Введите радиус окружности: ";
+    cin >> r;
+
+    okrujnost okrujnost(r);
+    cout << "Длина окружности: " << okrujnost.okr() << endl;
+    cout << "Площадь окружности: " << okrujnost.area() << endl;
+
+    return 0;
+}
